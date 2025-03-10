833bb938f60c8b693edf4fb6a635e2d73cb5ef57
Nabemh <nhanior@gmail.com>
2025-03-10 20:31:56 +0100

    coingecko init

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,20 +1,41 @@
+const { response } = require("express");
+
 document.addEventListener("DOMContentLoaded", () => {
     fetchGlobal();
 });
 
-function fetchGlobal(){
-
-}
-
+// Read data from localstorage but if?
 function getLocalStorageData(key){
     const storedData = localStorage.getItem(key);
     if(!storedData) return null;
 
     const parsedData = JSON.parse(storedData);
     const currentTime = Date.now();
+    //Data was older than 5  minutes Fetch it again
     if (currentTime - parsedData.timestamp > 300000){
         localStorage.removeItem(key);
         return null;
     }
     return parsedData.data;
 }
+
+function fetchGlobal(){
+    const localStoragekey = 'Global_data';
+    const localData = getLocalStorageData(localStoragekey);
+
+    if(localData){
+        displayGlobalData(localData);
+    }else {
+        const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+        fetch('https://api.coingecko.com/api/v3/global', options)
+            .then(response => response.json())
+            .then(data => {
+                const globalData = data.data;
+                displayGlobalData(data);
+                serLocalStorageData(localStoragekey, globalData);
+            })
+    }
+
+}
+
